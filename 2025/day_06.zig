0c548792214e30a5fe5fb0b042475cc195d18810
Perdit-io <marc_andrei_d_angeles@dlsu.edu.ph>
2026-02-18 15:19:58 +0800

    feat: finish day 6 code

--- 2025/day_06.zig
+++ 2025/day_06.zig
@@ -3,14 +3,10 @@ const std = @import("std");
 const Op = enum { add, multiply };
 
 fn doOperation(operator: Op, operand1: usize, operand2: usize) usize {
-    switch (operator) {
-        .add => {
-            return operand1 + operand2;
-        },
-        .multiply => {
-            return operand1 * operand2;
-        },
-    }
+    return switch (operator) {
+        .add => operand1 + operand2,
+        .multiply => operand1 * operand2,
+    };
 }
 
 pub fn part1(input: []const u8) usize {
@@ -18,12 +14,12 @@ pub fn part1(input: []const u8) usize {
     defer std.debug.assert(debug_allocator.deinit() == .ok);
     const allocator = debug_allocator.allocator();
 
-    var word_it = std.mem.tokenizeScalar(u8, input, '\n');
+    var line_it = std.mem.tokenizeScalar(u8, input, '\n');
 
     var lines = std.ArrayList(std.mem.TokenIterator(u8, .scalar)).empty;
     defer lines.deinit(allocator);
 
-    while (word_it.next()) |line| {
+    while (line_it.next()) |line| {
         const token_it = std.mem.tokenizeScalar(u8, line, ' ');
         lines.append(allocator, token_it) catch unreachable;
     }
@@ -49,6 +45,63 @@ pub fn part1(input: []const u8) usize {
 
         sum += current_result;
     }
+
+    return sum;
+}
+
+pub fn part2(input: []const u8) usize {
+    var debug_allocator: std.heap.DebugAllocator(.{}) = .init;
+    defer std.debug.assert(debug_allocator.deinit() == .ok);
+    const allocator = debug_allocator.allocator();
+
+    var line_it = std.mem.tokenizeScalar(u8, input, '\n');
+
+    var lines_al = std.ArrayList([]const u8).empty;
+    defer lines_al.deinit(allocator);
+
+    while (line_it.next()) |line| {
+        const trimmed_line = std.mem.trim(u8, line, "\r\t");
+        lines_al.append(allocator, trimmed_line) catch unreachable;
+    }
+
+    var sum: usize = 0;
+    var current_operator: Op = undefined;
+    var current_result: usize = 0;
+
+    const lines = lines_al.toOwnedSlice(allocator) catch unreachable;
+    defer allocator.free(lines);
+
+    for (lines[lines.len - 1], 0..) |operator_token, i| {
+        switch (operator_token) {
+            '+' => {
+                sum += current_result;
+                current_result = 0;
+                current_operator = Op.add;
+            },
+            '*' => {
+                sum += current_result;
+                current_result = 1;
+                current_operator = Op.multiply;
+            },
+            ' ' => {},
+            else => unreachable,
+        }
+
+        var current_number: usize = 0;
+
+        var not_blank: bool = false;
+
+        for (lines[0 .. lines.len - 1]) |line| {
+            if (line[i] == ' ') continue;
+            not_blank = true;
+            current_number *= 10;
+            current_number += line[i] - '0';
+        }
+
+        if (not_blank) current_result = doOperation(current_operator, current_result, current_number);
+    }
+
+    sum += current_result;
 
     return sum;
 }
